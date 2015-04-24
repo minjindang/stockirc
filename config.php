4f6df2f0c836f02d6c3f8de9cbbd8f5cb73153aa
willycode <w.illy741026@gmail.com>
2015-04-24 09:32:04 +0000

    BootStrap+PHP

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// defines database connection data
+define('DB_HOST', 'willycode-stockirc-1450031');
+define('DB_USER', 'willycode');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ajax');
+?>
