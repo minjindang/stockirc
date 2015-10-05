9db54facf46478489251ce4dc878115e83be22b7
root <root@e7128b67d9e7>
2015-10-05 14:28:46 +0800

    database config update

--- config.php
+++ config.php
@@ -1,7 +1,10 @@
 <?php
 // defines database connection data
-define('DB_HOST', 'willycode-stockirc-1450031');
-define('DB_USER', 'willycode');
-define('DB_PASSWORD', '');
+//define('DB_HOST', 'willycode-stockirc-1450031');
+//define('DB_USER', 'willycode');
+//define('DB_PASSWORD', '');
+define('DB_HOST', 'heroku_545421c4d4d2d18');
+define('DB_USER', 'b7667d4eba9530');
+define('DB_PASSWORD', 'f0177602');
 define('DB_DATABASE', 'ajax');
 ?>
